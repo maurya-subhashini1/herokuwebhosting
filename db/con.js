2cba8a19795167f4ea76695548a3e39ae7fce7d9
subhashini-maurya <subhashini20@navgurukul>
2022-03-05 23:29:48 +0530

    first commit

---
+++ db/con.js
@@ -0,0 +1,8 @@
+// const mongoose=require("mongoose");
+
+// mongoose.connect("mongodb://localhost:27017/mydatabase")
+// .then(()=>{
+//     console.log("connection successful");
+// }).catch(()=>{
+//     console.log("No connection");
+// })
